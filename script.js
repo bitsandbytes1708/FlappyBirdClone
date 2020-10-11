00883fceaee8071d51fe44df63ad423df5b28fba
Tal Bachar <44247475+TalBachar@users.noreply.github.com>
2020-10-11 14:53:34 +0000

    Graphics, Play again buttom
    Graphics: 
    # Added background to "You Lose" screen
    # Center the game, added border
    
    Game:
    # Added "Play again" button

--- script.js
+++ script.js
@@ -1,91 +1,107 @@
-var c = document.getElementById("canvas");
-var ctx = c.getContext("2d");
-var score;
-var widthOfGrass=c.width/40;
+let c = document.getElementById("canvas");
+let ctx = c.getContext("2d");
+let score;
+let widthOfGrass = c.width / 40;
+
 function drawGrass() {
-  for(var i = 0; i<40; i++) {
-    if(i%2==0) {
-      ctx.fillStyle="lightgreen"
+  for (let i = 0; i < 40; i++) {
+    if (i % 2 == 0) {
+      ctx.fillStyle = "lightgreen"
     } else {
-      ctx.fillStyle="green"
+      ctx.fillStyle = "green"
     }
-    ctx.fillRect(i*widthOfGrass, c.height-20, widthOfGrass,20)  
+    ctx.fillRect(i * widthOfGrass, c.height - 20, widthOfGrass, 20)
   }
 }
 function bounce() {
-  if(y>c.height-45) {
-    velocity=-4
+  if (y > c.height - 45) {
+    velocity = -4
     console.log("test")
   }
 }
 function makeArray() {
-  pipes=[]
-  for(var i = 0; i<100; i++) {
-    pipes[i]=0
+  pipes = []
+  for (let i = 0; i < 100; i++) {
+    pipes[i] = 0
   }
-  for(var i = 1; i<100; i++) {
-    interval=Math.random()*200+200;
-    pipes[i]=pipes[i-1]+interval
+  for (let i = 1; i < 100; i++) {
+    interval = Math.random() * 200 + 200;
+    pipes[i] = pipes[i - 1] + interval
   }
 }
 function makeHoles() {
-  holes=[]
-  for(var i = 0; i<100; i++) {
-    holes[i]=Math.random()*c.height-200
+  holes = []
+  for (let i = 0; i < 100; i++) {
+    holes[i] = Math.random() * c.height - 200
   }
 }
-collided=false;
+collided = false;
 function collisionDetection() {
-  for(var i = 0; i<100; i++) {
-    if(pipes[i]-tick<60 && pipes[i]-tick>-20) {
-      if(!(y>holes[i]+20 && y<holes[i]+180)) {
-        collided=true;
-        score=i;
+  for (let i = 0; i < 100; i++) {
+    if (pipes[i] - tick < 60 && pipes[i] - tick > -20) {
+      if (!(y > holes[i] + 20 && y < holes[i] + 180)) {
+        collided = true;
+        score = i;
       }
     }
   }
 }
 makeHoles();
-tick=-1000;
+tick = -1000;
 function drawPipes() {
-  tick=tick+1+Math.abs(tick/3000);
-  for(var i = 0; i<100; i++) {
-    ctx.fillStyle="darkgreen"
-    ctx.fillRect(pipes[i]-tick, 0, 50, c.height);
+  tick = tick + 1 + Math.abs(tick / 3000);
+  for (let i = 0; i < 100; i++) {
+    ctx.fillStyle = "darkgreen"
+    ctx.fillRect(pipes[i] - tick, 0, 50, c.height);
   }
-  for(var i = 0; i<100; i++) {
-    ctx.fillStyle="lightblue"
-    ctx.fillRect(pipes[i]-tick-1, holes[i], 52, 200)
+  for (let i = 0; i < 100; i++) {
+    ctx.fillStyle = "lightblue"
+    ctx.fillRect(pipes[i] - tick - 1, holes[i], 52, 200)
   }
 }
 makeArray();
 function drawFlappy() {
   ctx.beginPath();
-  ctx.fillStyle="yellow"
+  ctx.fillStyle = "yellow"
   ctx.arc(40, y, 20, 0, 2 * Math.PI);
   ctx.fill();
 }
-var y = c.height/2
-velocity=-2
+
+let y = c.height / 2
+velocity = -2
+
 function updatePosition() {
-  y+=velocity
-  velocity+=.1
+  y += velocity
+  velocity += .1
 }
+
 function clear() {
-  ctx.fillStyle="lightblue"
-  ctx.fillRect(0,0,c.width,c.height);
+  ctx.fillStyle = "lightblue"
+  ctx.fillRect(0, 0, c.width, c.height);
 }
+
 function lose() {
-  ctx.fillStyle="red"
-  ctx.fillRect(0,0,c.width,c.height)
-  ctx.font = "30px Arial";
-  ctx.fillStyle="black"
-  ctx.textAlign = "center";
-  ctx.fillText("You Lost", c.width/2, c.height/2);
-  ctx.fillText("Your score was: " + score, c.width/2, c.height/2+100);
+  //run background, write text on top
+  if (collided == true) {
+    const bg_image = new Image();
+    bg_image.src = "https://image.freepik.com/free-vector/sunburst-spiral-wallpaper_1284-3501.jpg"; //This picture is free for use, not Copyright needed
+    bg_image.onload = function () {
+      ctx.drawImage(bg_image, 0, 0, c.width, c.height);
+      ctx.font = "30px Verdana";
+      ctx.fillStyle = "black"
+      ctx.textAlign = "center";
+      ctx.fillText("You Lost", c.width / 2, c.height / 3);
+      ctx.fillText("Your score was: " + score, c.width / 2, c.height / 3 + 70);
+    }
+  }
+}
+
+function clickPlayagain() {
+  collided = false;
+  document.location.reload()
 }
 function gameloop() {
-  if(collided==false) {
+  if (collided == false) { // WIP
     clear();
     updatePosition();
     bounce();
@@ -99,9 +115,10 @@ function gameloop() {
   }
 }
 gameloop();
-window.addEventListener('keydown', function(event) {
-    const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
-    if(key == "ArrowUp") {
-      velocity=-3
-    }
-});
+
+// click to fly function
+window.addEventListener("click", flyFunction);
+function flyFunction() {
+  velocity = -3;
+}
+
